f472c4bd9a5c682b31c9732f4000271fb7d7c195
hojncode <glicm12@gmail.com>
2022-12-16 17:52:42 +0900

    AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { isDarkAtom } from "./atoms";
 import { darkTheme } from "./theme";
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -59,13 +59,15 @@ table {
   box-sizing: border-box;
 }
 body {
-  font-family: 'Source Sans Pro', sans-serif;
   font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  color:black;
   line-height: 1.2;
+  
 }
 a {
   text-decoration:none;
-  color: inherit;
+  color:inherit;
 }
 `;
 
