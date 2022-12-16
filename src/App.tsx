1dde3c2a195ecd7b3a14305e2adb3ed588994609
hojncode <glicm12@gmail.com>
2022-12-16 15:42:16 +0900

    MotionValues-3 : useViewportScroll, useTransform, useMotionValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import Router from "./Router";
 import { useEffect } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -14,28 +19,43 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgb(243, 239, 239);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  useEffect(() => {
-    scale.onChange(() => console.log(scale.get()));
-  }, [scale]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]); // useEffect(() => {
+  //   rotateZ.onChange(() => console.log(rotateZ.get()));
+  // }, [rotateZ]);
   // //! 박스 움직임 좌표 콘솔찍기
   // useEffect(() => {
   //   x.onChange(() => console.log(x.get()));
   // }, [x]);
-
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(19, 214, 235), rgb(0, 38, 255))",
+      "linear-gradient(135deg, rgb(235, 19, 152), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 255, 145), rgb(236, 172, 54))",
+    ]
+  );
+  // const { scrollY, scrollYProgress } = useViewportScroll();
+  // useEffect(() => {
+  //   scrollY.onChange(() => {
+  //     console.log(scrollY.get(), scrollYProgress.get());
+  //   });
+  // }, [scrollY, scrollYProgress]);
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
     <div>
       {/* <Router /> */}
-      <Wrapper>
+      <Wrapper style={{ background: gradient }}>
         {/* <button onClick={() => x.set(200)}>click me</button> */}
-        <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+        <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
       </Wrapper>
     </div>
   );
